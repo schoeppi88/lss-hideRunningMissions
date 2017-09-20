98d07b3d2a49c968a20c268712abac1ccbe121d7
Florian <31997785+schoeppi88@users.noreply.github.com>
2017-09-20 15:14:24 +0200

    Update lss-hideRunningMissions.user.js

--- lss-hideRunningMissions.user.js
+++ lss-hideRunningMissions.user.js
@@ -6,7 +6,7 @@
 // @include      *://www.leitstellenspiel.de/
 // @include      *://www.leitstellenspiel.de/*
 // @include      https://code.jquery.com/jquery-3.2.1.min.js
-// @grant        GM_log
+// @grant        none
 // @run          document-start
 // ==/UserScript==
 
